8941496418fffe9722f41f9e6243fa1e368d235a
Dmitriy Shibarshin <dima@shibarshin.com>
2013-08-12 21:58:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Woonu::Application.config.secret_key_base = '87523207bfa4c1b79c8923a66d24feb08ecbb089419404f989121b333cbe71890506fc4b7f33a7ef00f41495c21b6589b53fc68c83f5d83b9afc7558052abdb3'
